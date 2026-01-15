e2d35fcae8d06adc696c2fd0dbc1451d91b0cbcd
dan-kst <stupakdanielo@gmail.com>
2026-01-15 15:16:28 +0200

    feat: Implement the "/api/numbers" GET route.

---
+++ src/web/main_web.cpp
@@ -0,0 +1,39 @@
+#include "core/Factory.hpp"
+#include "crow.h"
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace Core;
+
+int
+main ()
+{
+  crow::SimpleApp app;
+  std::vector<std::unique_ptr<Pair> > dataStorage;
+
+  // Initial Seed Data
+  dataStorage.push_back (
+      NumberFactory::create (PairType::Degrees, First{ 90 }, Second{ 15 }));
+  dataStorage.push_back (
+      NumberFactory::create (PairType::Radians, First{ 1 }, Second{ 570 }));
+
+  /// @route GET /api/numbers
+  CROW_ROUTE (app, "/api/numbers")
+  ([&dataStorage] () {
+    std::vector<crow::json::wvalue> jsonList;
+    for (const auto &obj : dataStorage)
+      {
+        crow::json::wvalue item;
+        const auto type = obj->getType ();
+        item["type"] = (type == PairType::Degrees ? "Degrees" : "Radians");
+        item["display"] = obj->toString ();
+        item["first"] = static_cast<std::intmax_t> (obj->getFirst ());
+        item["second"] = static_cast<std::intmax_t> (obj->getSecond ());
+        jsonList.push_back (std::move (item));
+      }
+    return crow::json::wvalue (jsonList);
+  });
+
+  app.port (18080).loglevel (crow::LogLevel::Debug).multithreaded ().run ();
+}
